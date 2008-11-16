6d45621c1e9d9396b7c47a05304eb4c77e6c5db1
Piotr Andrzej Nowak <p.nowak2@gmail.com>
2008-11-16 21:06:50 +0100

    poprawki

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,47 +1,78 @@
 $(function(){
-      if (GBrowserIsCompatible()) {
-        var map = new GMap2($("#map_canvas")[0]);
-        var geocoder = new GClientGeocoder();
-        var center = new GLatLng(50.170693,18.904327);
-        var lastMarker = null;
-        
-        map.setCenter(center, 14);
 
-        GEvent.addListener(map, "click", function(overlay, latlng){
-          if(latlng != null){
-            var marker = lastMarker = new GMarker(latlng, {draggable: true});
-            map.addOverlay(marker);
-            geocoder.getLocations(latlng, showAddress);
-            
-            GEvent.addListener(marker, "click", function() {
-              marker.openInfoWindowHtml(marker.value);
-            });
-            
-            GEvent.addListener(marker, "dragstart", function() {
-              map.closeInfoWindow();
-            });
+  $("#search").click(function(){
+    searchAddress($("#place").val());
+  });
 
-            GEvent.addListener(marker, "dragend", function(latlng) {
-              lastMarker = marker;
-              geocoder.getLocations(latlng, showAddress);
-            });
-          }
-        });
-        
-        function showAddress(response) {
-          if (!response || response.Status.code != 200) {
-            alert("Status Code:" + response.Status.code);
+  if (GBrowserIsCompatible()) {
+    var map = new GMap2($("#map_canvas")[0]);
+    var geocoder = new GClientGeocoder();
+    var center = new GLatLng(50.170693,18.904327);
+    var lastMarker = null;
+    
+    map.openInfoWindow(center ,"Witaj w tripmate<br/>Kliknij aby wybrać lokalizację");
+    
+    map.setCenter(center, 14);
+
+    GEvent.addListener(map, "click", function(overlay, latlng){
+      if(latlng != null){
+        var marker = createMarker(latlng);
+        map.addOverlay(marker);
+        geocoder.getLocations(latlng, showAddress);
+       }
+    });
+         
+    function showAddress(response) {
+      if (!response || response.Status.code != 200) {
+        alert("Status Code:" + response.Status.code);
+      } else {
+        var place = response.Placemark[0];
+        point = new GLatLng(place.Point.coordinates[1],place.Point.coordinates[0]);
+        var address = '<b>Address:</b>' + place.address + '<br>' +
+                      '<b>Country code:</b> ' + place.AddressDetails.Country.CountryNameCode
+        lastMarker.openInfoWindowHtml(address);
+        lastMarker.value = address;
+        $("#places").html(address + "<br/>");
+      }
+    }
+    
+    function searchAddress(address) {
+      geocoder.getLatLng(
+        address,
+        function(point) {
+          if (!point) {
+            alert(address + " not found");
           } else {
-            var place = response.Placemark[0];
-            point = new GLatLng(place.Point.coordinates[1],place.Point.coordinates[0]);
-            var address = '<b>Address:</b>' + place.address + '<br>' +
-                          '<b>Country code:</b> ' + place.AddressDetails.Country.CountryNameCode
-            lastMarker.openInfoWindowHtml(address);
+            map.setCenter(point, 13);
+            var marker = createMarker(point);
+            map.addOverlay(marker);
+            marker.openInfoWindowHtml(address);
             lastMarker.value = address;
+            $("#places").slideDown().html(address + "<br/>");
           }
         }
-        
-      }
+      );
+    }
+    
+    function createMarker(latlng){
+      var marker = lastMarker = new GMarker(latlng, {draggable: true});
+      GEvent.addListener(marker, "click", function() {
+        marker.openInfoWindowHtml(marker.value);
+      });
+      
+      GEvent.addListener(marker, "dragstart", function() {
+        map.closeInfoWindow();
+      });
+
+      GEvent.addListener(marker, "dragend", function(latlng) {
+        lastMarker = marker;
+        geocoder.getLocations(latlng, showAddress);
+      });
+      
+      return marker;
+    }
+    
+  }
       
 
       
